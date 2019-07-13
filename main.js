95578944c735e4a3fd3c15f606cac5f578bb58fc
princeky07 <intern@outdoorsforyouth.org>
2019-07-13 13:57:39 -0600

    hamburgar

--- main.js
+++ main.js
@@ -1,8 +1,7 @@
-function myFunction() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
 }
